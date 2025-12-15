8f0d0baaa3c0246e0068dca3311aa8b6f036b322
michael <michael.sf36@yahoo.com.br>
2025-12-15 08:21:39 -0300

    update

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,7 @@
 import { isNotEmpty, IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
  
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
@@ -22,4 +23,9 @@ export class Postagem {
         onDelete: 'CASCADE',
     })
     tema: Tema;
-}
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {onDelete: 'CASCADE'})
+    usuario: Usuario
+    }
+
+
